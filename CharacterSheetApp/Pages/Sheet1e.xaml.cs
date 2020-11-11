b667640772479622f4e7cddcc2447cd78e022a65
Lasse Pedersen <lassep95@gmail.com>
2020-11-11 18:34:39 +0100

    - Base attack added  - Page with 1e sheet content added

---
+++ CharacterSheetApp/Pages/Sheet1e.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CharacterSheetApp.Pages
+{
+    /// <summary>
+    /// Interaction logic for Sheet1e.xaml
+    /// </summary>
+    public partial class Sheet1e : Page
+    {
+        public Sheet1e()
+        {
+            InitializeComponent();
+        }
+    }
+}
